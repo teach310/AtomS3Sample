ceac60d1f92b93cf78ec02572ce80f0bf6ede408
teach310 <okms310@yahoo.co.jp>
2023-07-01 23:42:02 +0900

    距離センサの値取得
    isRangeCompleteはずっとfalse帰ってきてしまった。理由わからず

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,33 @@
 #include <M5Unified.h>
+#include "Adafruit_VL53L0X.h"
 
-uint32_t count;
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 void setup()
 {
-
   auto cfg = M5.config();
   M5.begin(cfg);
   USBSerial.begin(115200);
-  M5.Display.setTextSize(3);
-  USBSerial.println("Hello World!!");
-  count = 0;
+  M5.Display.setTextSize(2);
+  Wire.setPins(SDA, SCL);
+  if (!lox.begin())
+  {
+    USBSerial.println("Failed to boot VL53L0X");
+    while (1)
+      ;
+  }
+  USBSerial.println("Start App\n\n");
+  lox.startRangeContinuous();
 }
 
 void loop()
 {
-
+  M5.Display.startWrite();
+  auto distance = lox.readRange();
+  M5.Display.clear(BLACK);
   M5.Display.setCursor(0, 20);
-  M5.Display.printf("COUNT: %d\n", count);
-  USBSerial.printf("COUNT: %d\n", count);
-  count++;
-  delay(1000);
+  M5.Display.printf("Distance\nmm: %d\n", distance);
+  M5.Display.endWrite();
+  USBSerial.println("Distance in mm: " + String(distance));
+  delay(200);
 }
